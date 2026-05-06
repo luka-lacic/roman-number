bce83bfa8e09f3d23055604adbaebab8b01c9314
Jatin <Jatin@LAPTOP-FJR6MDRL>
2026-05-06 12:57:29 +0200

    Conversione e stampa dei primi 500 numeri close #22

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -13,7 +13,8 @@ public class IntegerToRoman {
         }
         
         int[] values = {500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] symbols = {"D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String[] symbols = {"D", "CD", "C", "XC", "L", 
+        "XL", "X", "IX", "V", "IV", "I"};
         
         StringBuilder roman = new StringBuilder();
 
