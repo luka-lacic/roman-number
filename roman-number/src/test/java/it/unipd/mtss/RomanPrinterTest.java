b818ac0e462441e5ffbb2096bbddbd0356b90d8b
Jatin <Jatin@LAPTOP-FJR6MDRL>
2026-05-07 12:24:56 +0200

    Controllo stampa lettere romane close #34

--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -27,12 +27,12 @@ public class RomanPrinterTest {
     public void testPrint_WithNumberFive_ShouldReturnAsciiV() {
         String result = RomanPrinter.print(5);
         String expected = 
-            "__      __  \n" +
-            "\\ \\    / /  \n" +
-            " \\ \\  / /   \n" +
-            "  \\ \\/ /    \n" +
-            "   \\  /     \n" +
-            "    \\/      ";
+            "__      __ \n" +
+            "\\ \\    / / \n" +
+            " \\ \\  / /  \n" +
+            "  \\ \\/ /   \n" +
+            "   \\  /    \n" +
+            "    \\/     ";
         assertEquals(expected, result);
     }
     
@@ -53,12 +53,12 @@ public class RomanPrinterTest {
     public void testPrint_WithNumberFifty_ShouldReturnAsciiL() {
         String result = RomanPrinter.print(50);
         String expected = 
-            " _        \n" +
-            "| |       \n" +
-            "| |       \n" +
-            "| |       \n" +
-            "| |____   \n" +
-            "|______|  ";
+            " _       \n" +
+            "| |      \n" +
+            "| |      \n" +
+            "| |      \n" +
+            "| |____  \n" +
+            "|______| ";
         assertEquals(expected, result);
     }
 
@@ -79,28 +79,54 @@ public class RomanPrinterTest {
     public void testPrint_WithNumberFiveHundred_ShouldReturnAsciiD() {
         String result = RomanPrinter.print(500);
         String expected = 
-            " _____    \n" +
-            "|  __ \\   \n" +
-            "| |  | |  \n" +
-            "| |  | |  \n" +
-            "| |__| |  \n" +
-            "|_____/   ";
+            " _____   \n" +
+            "|  __ \\  \n" +
+            "| |  | | \n" +
+            "| |  | | \n" +
+            "| |__| | \n" +
+            "|_____/  ";
+        assertEquals(expected, result);
+    }
+    @Test
+    public void testPrint_WithNumberFourHundredFourtyFour_ShouldReturnAsciiCDXLIV() {
+        String result = RomanPrinter.print(444);
+        String expected = 
+            "  _____   _____   __   __  _        _____  __      __ \n" +
+            " / ____| |  __ \\  \\ \\ / / | |      |_   _| \\ \\    / / \n" +
+            "| |      | |  | |  \\ V /  | |        | |    \\ \\  / /  \n" +
+            "| |      | |  | |   > <   | |        | |     \\ \\/ /   \n" +
+            "| |____  | |__| |  / . \\  | |____    | |      \\  /    \n" +
+            " \\_____| |_____/  /_/ \\_\\ |______|  |___|      \\/     ";
         assertEquals(expected, result);
     }
 
-    // Controllare gli spazi
-    // @Test
-    // public void testPrint_WithNumberNinehundredNinetynine_ShouldReturnAsciiCMXCIX() {
-    //     String result = RomanPrinter.print(999);
-    //     String expected = 
-    //         "  _____   __  __  __   __   _____   _____  __   __ \n" +
-    //         " / ____| |  \\/  | \\ \\ / /  / ____| |_   _| \\ \\ / / \n" +
-    //         "| |      | \\  / |  \\ V /  | |        | |    \\ V /  \n" +
-    //         "| |      | |\\/| |   > <   | |        | |     > <   \n" +
-    //         "| |____  | |  | |  / . \\  | |____    | |    / . \\  \n" +
-    //         " \\_____| |_|  |_| /_/ \\_\\  \\_____|  |___|  /_/ \\_\\ \n";
-    //     assertEquals(expected, result);
-    // }
+    @Test
+    public void testPrint_WithNumberEightHundredEightyEight_ShouldReturnAsciiDCCCLXXXVIII() {
+        String result = RomanPrinter.print(888);
+
+        String expected = 
+            " _____     _____    _____    _____   _       __   __ __   __ __   __ __      __  _____   _____   _____  \n" +
+            "|  __ \\   / ____|  / ____|  / ____| | |      \\ \\ / / \\ \\ / / \\ \\ / / \\ \\    / / |_   _| |_   _| |_   _| \n" +
+            "| |  | | | |      | |      | |      | |       \\ V /   \\ V /   \\ V /   \\ \\  / /    | |     | |     | |   \n" +
+            "| |  | | | |      | |      | |      | |        > <     > <     > <     \\ \\/ /     | |     | |     | |   \n" +
+            "| |__| | | |____  | |____  | |____  | |____   / . \\   / . \\   / . \\     \\  /      | |     | |     | |   \n" +
+            "|_____/   \\_____|  \\_____|  \\_____| |______| /_/ \\_\\ /_/ \\_\\ /_/ \\_\\     \\/      |___|   |___|   |___|  ";
+            
+        assertEquals(expected, result);
+    }
+    
+    @Test
+    public void testPrint_WithNumberNineHundredNinetyNine_ShouldReturnAsciiCMXCIX() {
+        String result = RomanPrinter.print(999);
+        String expected = 
+            "  _____   __  __  __   __   _____   _____  __   __ \n" +
+            " / ____| |  \\/  | \\ \\ / /  / ____| |_   _| \\ \\ / / \n" +
+            "| |      | \\  / |  \\ V /  | |        | |    \\ V /  \n" +
+            "| |      | |\\/| |   > <   | |        | |     > <   \n" +
+            "| |____  | |  | |  / . \\  | |____    | |    / . \\  \n" +
+            " \\_____| |_|  |_| /_/ \\_\\  \\_____|  |___|  /_/ \\_\\ ";
+        assertEquals(expected, result);
+    }
 
     @Test
     public void testPrint_WithNumberOneThousand_ShouldReturnAsciiM() {
